b8c16617d145e3d4434291216442a542b56509c2
gsico <goran22sico@gmail.com>
2021-01-03 17:15:27 +0100

    adding retrofit hilt dependency

---
+++ app/src/main/java/com/sicoapp/movieapp/di/RetrofitModule.kt
@@ -0,0 +1,42 @@
+package com.sicoapp.movieapp.di
+
+import com.sicoapp.movieapp.data.api.MovieApiService
+import com.sicoapp.movieapp.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * @author ll4
+ * @date 1/3/2021
+ */
+
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .client(okHttpClient)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): MovieApiService =
+        retrofit.create(MovieApiService::class.java)
+}
